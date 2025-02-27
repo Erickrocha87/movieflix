9c0f20fd97c181b7058c2794b3ea093dc065d98d
erickrocha87 <ericksaggi31@gmail.com>
2025-02-27 11:40:43 -0300

    feat: created findMovieByCategory

--- src/main/java/com/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/movieflix/repositories/MovieRepository.java
@@ -1,7 +1,15 @@
 package com.movieflix.repositories;
 
+import com.movieflix.entity.Category;
 import com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategory(List<Category> categoriesId);
+
 }
