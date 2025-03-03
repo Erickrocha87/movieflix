045dc8489d5f9b85f6da022843da40b81c97b28f
erickrocha87 <ericksaggi31@gmail.com>
2025-03-03 12:52:23 -0300

    feat: created register with cryptography for password

--- src/main/java/com/movieflix/services/UserService.java
+++ src/main/java/com/movieflix/services/UserService.java
@@ -7,16 +7,20 @@ import com.movieflix.entity.User;
 import com.movieflix.mapper.UserMapper;
 import com.movieflix.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     public UserResponseDTO register (UserRequestDTO request) {
         User user = UserMapper.map(request);
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
         return UserMapper.map(user);
     }
