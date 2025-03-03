9d82c2ca6b7fb2a4115aaf12efc78264b935cdad
erickrocha87 <ericksaggi31@gmail.com>
2025-03-03 13:00:54 -0300

    feat: implements UserDetailsService

---
+++ src/main/java/com/movieflix/services/AuthService.java
@@ -0,0 +1,20 @@
+package com.movieflix.services;
+
+import com.movieflix.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User or password is incorrect"));
+    }
+}
